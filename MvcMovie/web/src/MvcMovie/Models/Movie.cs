960c5330e875499336ea28db2a75633c8eb59748
Nate Amundson <naamunds@microsoft.com>
2016-07-13 14:32:17 -0500

    Add Docker Compose functionality

--- MvcMovie/web/src/MvcMovie/Models/Movie.cs
+++ MvcMovie/web/src/MvcMovie/Models/Movie.cs
@@ -1,13 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
+    [Table("movie")]
     public class Movie
     {
+        [Column("id")]
         public int ID { get; set; }
+        [Column("title")]
         public string Title { get; set; }
+        [Column("releasedate")]
         public DateTime ReleaseDate { get; set; }
+        [Column("genre")]
         public string Genre { get; set; }
+        [Column("price")]
         public decimal Price { get; set; }
     }
 }
